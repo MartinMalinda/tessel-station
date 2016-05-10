b8c3fc2dd0e44b59d8666388aa1e2b0bcbf6a986
martinmalinda <malindacz@gmail.com>
2016-05-10 23:05:11 +0200

    make chart width dynamic

--- app/index/controller.js
+++ app/index/controller.js
@@ -5,45 +5,23 @@ const {computed} = Ember;
 export default Ember.Controller.extend({
   today: moment(),
 
-  chartData: computed('model.today.measurements.[]', function(){
-    console.log(this.get('model.today.measurements').mapBy('value'));
-    return {
-      datasets: {
-        data: this.get('model.today.measurements').mapBy('value'),
-        label: 'test',
-        fill: true
-      },
-      labels: this.get('model.today.measurements').mapBy('type.unit')
-    }
+  chartWidth: computed('model.today.measurements.[]',function(){
+    return this.get('model.today.measurements.length') * 50;
   }),
 
-  chartData2: computed('model.today.measurements.[]', function(){
+  chartData: computed('model.today.measurements.[]', function(){
     return  {
     labels: this.get('model.today.measurements').mapBy('time'),
     datasets: [
         {
-            label: "My First dataset",
-            fill: false,
-            lineTension: 0.1,
-            backgroundColor: "rgba(75,192,192,0.4)",
-            borderColor: "rgba(75,192,192,1)",
-            borderCapStyle: 'butt',
-            borderDash: [],
-            borderDashOffset: 0.0,
-            borderJoinStyle: 'miter',
-            pointBorderColor: "rgba(75,192,192,1)",
-            pointBackgroundColor: "#fff",
-            pointBorderWidth: 1,
-            pointHoverRadius: 5,
-            pointHoverBackgroundColor: "rgba(75,192,192,1)",
-            pointHoverBorderColor: "rgba(220,220,220,1)",
-            pointHoverBorderWidth: 2,
-            pointRadius: 1,
-            pointHitRadius: 10,
             data: this.get('model.today.measurements').mapBy('value'),
         }
     ]};
   }),
+
+  chartOptions: {
+    animation: false
+  },
 
   actions: {
     addValue(){
